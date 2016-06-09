5bf09038bd993e9b732ebc12d74457451d386118
jongbu <josephsherpa7@gmail.com>
2016-06-09 11:00:52 -0400

    2nd commit

--- script.js
+++ script.js
@@ -10,6 +10,30 @@ $(document).ready(function(){
       url: 'https://api.spotify.com/v1/search',
       data: formData,
       success: function (response) {
+        var artistArray= response.artists.items; 
+        for(var i = 0 ; i<artistArray.length; i++){
+          for(var j = 0; j<artistArray[i].images.length; j++){
+            document.getElementById("images").innerHTML+="<img src=\""+artistArray[i].images[j].url+"\">";
+            console.log(artistArray[i].images[j].url);
+          }
+        }
+      }
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#search").on("submit",function(e){
+    e.preventDefault();
+    var formData = {
+      'q'              : $('input[name=q]').val(),
+      'type'             : $('input[name=type]').val()
+    };
+    $.ajax({
+      type: "GET",
+      url: 'https://api.spotify.com/v1/search',
+      data: formData,
+      success: function (response) {
         console.log(response.artists.items);
         var artists = response.artists.items;
         for (var i =0; i<artists.length;i++){
@@ -19,3 +43,5 @@ $(document).ready(function(){
     });
   });
 });
+
+
